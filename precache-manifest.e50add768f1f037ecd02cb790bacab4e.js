ce69c23c18cc76368766857215a39ac31a6a3f12
Joke Heyndels <jo_hannah@outlook.com>
2020-04-09 16:13:23 +0200

    Updates

---
+++ precache-manifest.e50add768f1f037ecd02cb790bacab4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65126740a0e5d469958c426813f8c07a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c978fd6e5d42f23e97ad",
+    "url": "/monsters-rolodex/static/css/main.2b5834ac.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c978fd6e5d42f23e97ad",
+    "url": "/monsters-rolodex/static/js/main.a911c88e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
